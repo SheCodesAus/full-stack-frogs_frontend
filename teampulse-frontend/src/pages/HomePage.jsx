5c28db3e1e9a25be2d2cd078dab1543c0538c355
Sara <leungyunyee@gmail.com>
2025-11-20 16:21:05 +0800

    created button component v1

--- teampulse-frontend/src/pages/HomePage.jsx
+++ teampulse-frontend/src/pages/HomePage.jsx
@@ -2,28 +2,24 @@ import { useNavigate } from 'react-router-dom';
 import './HomePage.css';
 
 import Logo from '../components/Logo'
+import Button from '../components/ButtonComponent';
 
 function HomePage() {
     const navigate = useNavigate();
 
     return (
-        <>
-            <Logo size={300} />
-            <div className='welcoming-content'>
-                <div className='tagline'>
-                    <h3>Take a breath, </h3>
-                    <h3>You’re in a safe space. </h3>
-                </div>
-                <div className='direct-btns'>
-                    <div>
-                        <button className="login-btn" onClick={() => navigate('/login')}>Login</button>
-                    </div>
-                    <div>
-                        <button className="signup-btn" onClick={() => navigate('/signup')}>Signup</button>
-                    </div>
-                </div>
+        <section className='landing'>
+            <header className='header'>
+                <Logo size={290} />
+            </header>
+            <div className='tagline'>
+                <h3>Take a breath, </h3>
+                <h3>You’re in a safe space. </h3>
             </div>
-        </>
+            <Button text={'LOGIN'} width={'50vw'} to="/login" />
+            <Button text={'SIGNUP'} width={'50vw'} to="/login" />
+
+        </section >
     )
 }
 
