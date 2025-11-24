cb0aaea7817f9f5b3d1d2b117cf2fc47d3030e20
Sara <leungyunyee@gmail.com>
2025-11-24 23:56:11 +0800

    Add Logo to LoginPage and SignupPage

--- teampulse-frontend/src/pages/SignupPage.jsx
+++ teampulse-frontend/src/pages/SignupPage.jsx
@@ -1,10 +1,12 @@
 import SignupForm from "../components/SignupForm";
 import "./SignupPage.css";
+import Logo from "../components/Logo";
 
 function SignupPage({ onNavigateToLogin }) {
     return (
         <div className="signup-page-wrapper">
             <div className="signup-card">
+                <header className='header'><Logo size={290} /></header>
 
                 <h2 className="signup-title">
                     Create your account ✨
