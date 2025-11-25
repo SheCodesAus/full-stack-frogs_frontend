8ff31d106ee92cf79fb824124cdbc24802cb12b8
BLOSSOM <ELIZA.HERBISON94@GMAIL.COM>
2025-11-25 19:52:22 +0800

    workload pie chart component and dashboard updates wip

---
+++ teampulse-frontend/src/mocks/mockWorkloads.js
@@ -0,0 +1,15 @@
+export const mockWorkloads = [
+    { value: 1, description: "All Good" },
+    { value: 2, description: "Busy But Fine" },
+    { value: 3, description: "Quite Busy" },
+    { value: 4, description: "Very Busy" }
+];
+
+export const mockPulseLogs = [
+    { user: 1, workload: 2 },
+    { user: 2, workload: 3 },
+    { user: 3, workload: 4 },
+    { user: 4, workload: 2 },
+    { user: 5, workload: 1 },
+    { user: 6, workload: 3 }
+];
--- teampulse-frontend/src/pages/DashboardPage.jsx
+++ teampulse-frontend/src/pages/DashboardPage.jsx
@@ -1,5 +1,12 @@
+import WorkloadPieChart from "../components/WorkLoadPieChart";
+
 function DashboardPage() {
-    return <h1>This is a dashboard.</h1>;
+    return (
+    <div><h1>Team Dashboard</h1>
+        {/* Pie Chart Section */}
+            <WorkloadPieChart/>
+    </div>
+    );
 }
 
 export default DashboardPage;
