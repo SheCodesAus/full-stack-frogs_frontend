f24983980a2951ce3cbd1669e128412baac5ab0d
Sara <leungyunyee@gmail.com>
2025-11-27 17:54:11 +0800

    Created dashboard page, DashboardButton Component, Mock files for teams and PulseLogs data, Created Logo v2, Set breathing backgroud to all pages

--- teampulse-frontend/src/components/WeeklyComparison.jsx
+++ teampulse-frontend/src/components/WeeklyComparison.jsx
@@ -5,47 +5,54 @@ import Button from './ButtonComponent';
 
 // #Data simulation
 const moodData = [
-    { week: 'Week 1', mood: 1.72, prevPeriod: 3 },
-    { week: 'Week 2', mood: 3.04, prevPeriod: 3.5 },
-    { week: 'Week 3', mood: 2.32, prevPeriod: 1.8 },
-    { week: 'Current Week', mood: 3.16, prevPeriod: 2.9 },
+    { week: 'Current', currentPeriod: 3.16, prevPeriod: 2.9 },
+    { week: 'Week 32', currentPeriod: 1.72, prevPeriod: 3 },
+    { week: 'Week 33', currentPeriod: 3.04, prevPeriod: 3.5 },
+    { week: 'Week 34', currentPeriod: 2.32, prevPeriod: 1.8 },
 ];
 
 const workflowData = [
-    { week: 'Week 1', workflow: 6.8, prevPeriod: 7.5 },
-    { week: 'Week 2', workflow: 5.5, prevPeriod: 7.0 },
-    { week: 'Week 3', workflow: 7.1, prevPeriod: 6.5 },
-    { week: 'Current Week', workflow: 8.5, prevPeriod: 8.0 },
+    { week: 'Current', workflow: 8.5, prevPeriod: 8.0 },
+    { week: 'Week 32', workflow: 6.8, prevPeriod: 7.5 },
+    { week: 'Week 33', workflow: 5.5, prevPeriod: 7.0 },
+    { week: 'Week 34', workflow: 7.1, prevPeriod: 6.5 },
 ];
-// #endregion
-
+const moodLabels = {
+    1: "Angry",
+    2: "Anxious",
+    3: "Calm",
+    4: "Motivated",
+};
 const LineChartExample = ({ isAnimationActive = true }) => (
 
     <div className='weeklyComparisonContainer'>
         <div className='weekly-header'>
-            <h2>Weekly trend for</h2>
-            <Button text={'Mood'} width={'18vw'} />
-            <Button text={'Workflow'} width={'18vw'} />
+            <h2 className='headline'>Weekly trend for <strong>Average Team Mood </strong></h2>
         </div>
 
         <LineChart
             style={{ width: '85%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.68, padding: '6%' }}
             responsive
             data={moodData}
-            margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-            }}
         >
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="week" />
-            <YAxis width="auto" domain={[0, 4]} />
+            <YAxis width="auto" domain={[1, 4]}  
+                ticks={[1, 2, 3, 4]}
+                tickFormatter={(value) => moodLabels[value]} tickLine={false} axisLine={false}   padding={{bottom: 35 }} />
             <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="mood" stroke="#82ca9d" strokeWidth={2} dot={{ r: 5, strokeWidth: 2, fill: '#fff' }} isAnimationActive={isAnimationActive} />
-            <Line type="monotone" dataKey="prevPeriod" stroke="#8884d8" strokeWidth={2} strokeDasharray="3 3" dot={{ r: 2, strokeWidth: 1 }}
+            <Legend
+                verticalAlign="top"
+                align="right"
+                height={34}
+                iconType="circle"
+                wrapperStyle={{
+                    top: 35,
+                    right: 15
+                }}
+            />
+            <Line type="monotone" name="Current Period" dataKey="currentPeriod" stroke="#82ca9d" strokeWidth={2} dot={{ r: 5, strokeWidth: 2, fill: '#fff' }} isAnimationActive={isAnimationActive} />
+            <Line type="monotone" name="Previous Period" dataKey="prevPeriod" stroke="#8884d8" strokeWidth={2} strokeDasharray="3 3" dot={{ r: 2, strokeWidth: 1 }}
                 isAnimationActive={isAnimationActive} />
         </LineChart>
 
--- teampulse-frontend/src/main.jsx
+++ teampulse-frontend/src/main.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import './styles/global.css'
+import { useState } from "react";
+import  React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./components/AuthProvider.jsx";
+import './styles/global.css'
 
 import HomePage from "./pages/HomePage.jsx";
 import SignupPage from "./pages/SignupPage.jsx";
@@ -10,6 +11,7 @@ import LoginPage from "./pages/LoginPage.jsx";
 import UserFormPage from "./pages/UserFormPage.jsx";
 import QuotePage from "./pages/QuotePage.jsx";
 import DashboardPage from "./pages/DashboardPage.jsx";
+import LandingAnimation from "./components/LandingAnimation";
 
 
 const router = createBrowserRouter([
@@ -21,9 +23,21 @@ const router = createBrowserRouter([
     { path: "/dashboard", element: <DashboardPage /> },
 ]);
 
+// function AppRoot() {
+//   const [showIntro, setShowIntro] = useState(true);
+
+//   return showIntro ? (
+//     <LandingAnimation onFinish={() => setShowIntro(false)} />
+//   ) : (
+//     <RouterProvider router={router} />
+//   );
+// }
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
       <AuthProvider>
+        {/* <AppRoot /> */}
         <RouterProvider router={router} />
       </AuthProvider>
   </React.StrictMode>
