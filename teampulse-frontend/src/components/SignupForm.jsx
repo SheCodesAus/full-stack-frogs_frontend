7d6e1c1dcbff6baf8c2ed57b9ff77c208e4c94c1
Sara <leungyunyee@gmail.com>
2025-12-02 19:30:51 +0800

    Changing file name agiannnnn

--- teampulse-frontend/src/components/SignupForm.jsx
+++ teampulse-frontend/src/components/SignupForm.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./signupform.css";
+import "./SignupForm.css";
 import  getTeams  from "../api/get-teams";
 import postSignup from "../api/post-signup";
 
