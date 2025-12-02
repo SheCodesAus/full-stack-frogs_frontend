261052ae277ff0a5ed0e7622601c9027c66d0456
Joyce Gonzaga <gonzaga.joyce015@gmail.com>
2025-12-02 18:26:05 +0800

    updates

---
+++ teampulse-frontend/src/hooks/use-checkins.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import getAllCheckIns from "../api/get-all-checkins";
+
+export default function useCheckIns() {
+    const [checkins, setCheckins] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        getAllCheckIns()
+            .then((data) => {
+                setCheckins(data);
+            })
+            .catch((err) => {
+                setError(err.message);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }, []);
+
+    return { checkins, isLoading, error };
+}
